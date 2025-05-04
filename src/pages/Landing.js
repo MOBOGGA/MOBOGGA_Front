448344c7b35d6972bd203cd906f79220beec9e33
lingardd09 <sherlockicker@gmail.com>
2025-05-04 20:22:28 +0900

    feat: 구글로 시작하기 버튼을 눌렀을 때 '/login'으로 이동하기

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import styles from './styles/Landing.module.css';
 import GoogleStartBtnDefault from '../assets/GoogleStartBtn-Default.svg';
 import GoogleStartBtnHover from '../assets/GoogleStartBtn-Hover.svg';
 
 function Landing() {
   const [isHovering, setIsHovering] = useState(false);
+  const navigate = useNavigate();
 
   const onMouseOver = () => {
     setIsHovering(true);
@@ -14,6 +16,10 @@ function Landing() {
     setIsHovering(false);
   };
 
+  const onClickGoogleStartBtn = () => {
+    navigate(`/login`);
+  };
+
   return (
     <>
       <div className={styles.body}>
@@ -21,8 +27,8 @@ function Landing() {
           교내 공연 정보와 동아리 정보 확인은 모두 <span className={styles.landing_words_highlight}>모보까</span>에서
         </div>
         <div className={styles.google_start_btn_box}>
-          <img className={styles.google_start_btn_box} src={isHovering ? GoogleStartBtnHover : GoogleStartBtnDefault} alt="google-start-btn" onMouseOver={onMouseOver}
-          onMouseOut={onMouseOut} />
+          <img className={styles.google_start_btn} src={isHovering ? GoogleStartBtnHover : GoogleStartBtnDefault} alt="google-start-btn" onMouseOver={onMouseOver}
+          onMouseOut={onMouseOut} onClick={onClickGoogleStartBtn}/>
         </div>
       </div>
 
