66ca680825bc5102a55c12e7a5f0b98ac88b14cc
lingardd09 <sherlockicker@gmail.com>
2025-05-06 06:23:45 +0900

    feat: App.js에 로그인 페이지 연결

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 // import Main from "./pages/Main";
 import Landing from "./pages/Landing";
+import Login from './pages/Login'
 
 import './App.css';
 
@@ -14,6 +15,7 @@ function App() {
       <Header />
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       <Footer />
     </div>
