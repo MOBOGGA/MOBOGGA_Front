00913d80e7940852badb62ad82df1813a6041946
lingardd09 <sherlockicker@gmail.com>
2025-05-08 23:51:57 +0900

    feat: 로그인 페이지 개발 완료

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import styles from './styles/Landing.module.css';
-import GoogleStartBtnDefault from '../assets/GoogleStartBtn-Default.svg';
-import GoogleStartBtnHover from '../assets/GoogleStartBtn-Hover.svg';
-import LandingPageTape from '../assets/LandingPageTape.svg'
-import LandingPageWords from '../assets/landingPageWords.svg'
+import { useNavigate } from "react-router-dom";
+import styles from "./styles/Landing.module.css";
+import GoogleStartBtnDefault from "../assets/GoogleStartBtn-Default.svg";
+import GoogleStartBtnHover from "../assets/GoogleStartBtn-Hover.svg";
+import LandingPageTape from "../assets/LandingPageTape.svg";
+import LandingPageWords from "../assets/landingPageWords.svg";
 
 function Landing() {
   const [isHovering, setIsHovering] = useState(false);
@@ -19,7 +19,7 @@ function Landing() {
   };
 
   const onClickGoogleStartBtn = () => {
-    navigate(`/login`);
+    navigate(`/구글 로그인 페이지`);
   };
 
   return (
@@ -30,15 +30,20 @@ function Landing() {
             <img className={styles.words_img} src={LandingPageWords} alt="" />
           </div>
           <div className={styles.google_start_btn_box}>
-            <img className={styles.google_start_btn} src={isHovering ? GoogleStartBtnHover : GoogleStartBtnDefault} alt="google-start-btn" onMouseOver={onMouseOver}
-            onMouseOut={onMouseOut} onClick={onClickGoogleStartBtn}/>
+            <img
+              className={styles.google_start_btn}
+              src={isHovering ? GoogleStartBtnHover : GoogleStartBtnDefault}
+              alt="google-start-btn"
+              onMouseOver={onMouseOver}
+              onMouseOut={onMouseOut}
+              onClick={onClickGoogleStartBtn}
+            />
           </div>
         </div>
         <div className={styles.tape_box}>
           <img className={styles.landing_tape} src={LandingPageTape} alt="" />
         </div>
       </div>
-
     </>
   );
 }
