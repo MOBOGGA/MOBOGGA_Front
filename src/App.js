2a128073cdee3c0031a776f8b6b994e8ac03f786
lingardd09 <sherlockicker@gmail.com>
2025-05-09 00:33:52 +0900

    add: 추가정보기입 페이지 js 및 css 생성, App.js 연결 완료

--- src/App.js
+++ src/App.js
@@ -2,30 +2,31 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import Main from "./pages/Main";
 
 import Landing from "./pages/Landing";
 import Show from "./pages/Show";
-import Login from './pages/Login'
+import Login from "./pages/Login";
+import AddInfo from "./pages/AddInfo";
 
 import "./App.css";
 
 function App() {
   return (
-  <BrowserRouter>
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/show" element={<Show />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <Footer />
-    </div>
-  </BrowserRouter>
-
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/show" element={<Show />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/add-info" element={<AddInfo />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/AddInfo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./styles/AddInfo.module.css";
+
+
+function AddInfo() {
+  return (
+    <>
+      <div className={styles.body}>
+        츄가 정보 페이지
+      </div>
+    </>
+  );
+}
+
+export default AddInfo;
