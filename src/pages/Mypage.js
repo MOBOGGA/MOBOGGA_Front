1c5ea40474710d88f7ccca0e97114935c6051351
lingardd09 <sherlockicker@gmail.com>
2025-05-11 00:29:23 +0900

    add: 마이페이지 js 및 css 생성

---
+++ src/pages/Mypage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./styles/Mypage.module.css";
+
+
+function Mypage() {
+  return (
+    <>
+      <div className={styles.body}>
+        mypage
+      </div>
+    </>
+  );
+}
+
+export default Mypage;
