4da85abdef8c374fdeccd890f64021cd38923488
lingardd09 <sherlockicker@gmail.com>
2025-05-12 15:32:33 +0900

    add: 개인정보수정페이지 js 및 css 생성

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Show from "./pages/Show";
 import Login from "./pages/Login";
 import AddInfo from "./pages/AddInfo";
 import Mypage from "./pages/Mypage"
+import UpdateProfile from "./pages/UpdateProfile";
 
 import "./App.css";
 
@@ -25,6 +26,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/add-info" element={<AddInfo />} />
           <Route path='/mypage' element={<Mypage />} />
+          <Route path="/mypage/update" element={<UpdateProfile />} />
         </Routes>
         <Footer />
       </div>
---
+++ src/pages/UpdateProfile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./styles/UpdateProfile.module.css";
+
+
+function UpdateProfile() {
+  return (
+    <>
+      <div className={styles.body}>
+        프로필 정보 수정
+      </div>
+    </>
+  );
+}
+
+export default UpdateProfile;
