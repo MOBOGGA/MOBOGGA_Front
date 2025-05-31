33dd073fed8b9e47935e6194b26a7edbe86fc94e
lingardd09 <sherlockicker@gmail.com>
2025-05-31 22:22:47 +0900

    feat: 공연예매내역카드 80% 완료

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./styles/Mypage.module.css";
 
+import poster from "../assets/mainTest/3.png";
+
 function Mypage() {
   const navigate = useNavigate();
   const userId = localStorage.getItem("userId");
@@ -66,9 +68,9 @@ function Mypage() {
         });
       } catch (error) {
         console.error("Error fetching user profile:", error);
-      //   setError(error.message);
-      // } finally {
-      //   setIsLoading(false);
+        //   setError(error.message);
+        // } finally {
+        //   setIsLoading(false);
       }
     };
 
@@ -131,7 +133,39 @@ function Mypage() {
         </div>
         <div className={styles.container}>
           <div className={styles.reservlist_title}>공연 예매 내역</div>
-          <div className={styles.card}></div>
+          <div className={styles.card}>
+            <div className={styles.card_img_box}>
+              <img className={styles.card_img} src={poster} alt="공연 이미지" />
+            </div>
+            <div className={styles.card_text_box}>
+              <div className={styles.card_title}>우리 집에 왜 왔니?</div>
+              <div className={styles.card_info_box}>
+                <div className={styles.card_content}>
+                  <div className={styles.card_info_header}>1공: </div>
+                  <div className={styles.card_date}>2025.05.23(금) 19시 00분</div>
+                </div>
+                <div className={styles.card_content}>
+                  <div className={styles.card_info_header}>장소: </div>
+                  <div className={styles.card_place}>학관 102호</div>
+                </div>
+                <div className={styles.card_content}>
+                  <div className={styles.card_info_header}>담당자: </div>
+                  <div className={styles.card_manager}>010-1234-5678(김이름)</div>
+                </div>
+                <div className={styles.card_content}>
+                  <div className={styles.card_info_header}>계좌번호: </div>
+                  <div className={styles.card_account}>
+                    ㅇㅇ은행 0000-0000-0000-00
+                  </div>
+                </div>
+                <div className={styles.ticket_info}>
+                  <div className={styles.ticket_num}>4매</div>
+                  <div className={styles.ticket_price}>18000원</div>
+                  <div className={styles.deposit_status}>미입금</div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className={styles.card}></div>
           <div className={styles.card}></div>
           <div className={styles.card}></div>
