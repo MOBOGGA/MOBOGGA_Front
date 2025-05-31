3606e4c19baa1d3c3049cd2cabab3d560dccdc96
Haeun <hahashe@handong.ac.kr>
2025-06-01 00:27:18 +0900

    feat : 동아리 페이지 생성 및 디자인 완료

--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./styles/Main.module.css";
 
+import ClubList from "../components/ClubList";
 
 function Clubs() {
   return (
     <>
-      <div className={styles.clubs}>
-        Clubs page
+      <div className={styles.main}>
+        <span className={styles.categoryText}>카테고리</span>
+        <ClubList/>
       </div>
     </>
   );
