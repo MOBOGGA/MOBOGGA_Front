347e58d94b680b845d68d865544b84854aa22092
lingardd09 <sherlockicker@gmail.com>
2025-05-31 22:33:58 +0900

    feat: 구글로그인버튼 클릭 시 반응하는 코드 구현

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,7 +18,38 @@ function Login() {
   };
 
   const onClickGoogleLoginBtn = () => {
-    navigate(`/add-info`);
+    // CSRF 방지를 위한 state 생성
+    const state = Math.random().toString(36).substring(2);
+    sessionStorage.setItem("oauth_state", state);
+
+    // nonce 생성
+    const nonce =
+      Math.random().toString(36).substring(2) + Date.now().toString(36);
+
+    // URL 파라미터 설정
+    const authUrl = new URL("https://accounts.google.com/o/oauth2/v2/auth");
+    console.log("id: " + process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID);
+    console.log("uri: " + process.env.REACT_APP_GOOGLE_AUTH_REDIRECT_URI);
+    console.log("id: " + process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID);
+
+    // 환경변수에서 값을 가져와 URL에 추가
+    authUrl.searchParams.append(
+      "client_id",
+      process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID
+    );
+    authUrl.searchParams.append(
+      "redirect_uri",
+      process.env.REACT_APP_GOOGLE_AUTH_REDIRECT_URI
+    );
+    authUrl.searchParams.append("response_type", "id_token");
+    authUrl.searchParams.append("scope", "email profile openid");
+    authUrl.searchParams.append("nonce", nonce);
+    authUrl.searchParams.append("access_type", "offline");
+    authUrl.searchParams.append("prompt", "consent");
+    authUrl.searchParams.append("state", state);
+
+    window.location.href = authUrl.toString();
+    // navigate(`/add-info`);
   };
 
   return (
