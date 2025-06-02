67e2f62867fcdc7157117dbd3d340be8580cd171
Haeun <hahashe@handong.ac.kr>
2025-06-03 03:10:50 +0900

    feat : clubdetail 페이지 card 생성

---
+++ src/components/ClubDetail/EventCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "../styles/ShowCard.module.css";
+
+
+
+function EventCard({ show }) {
+  return (
+    <div className={styles.showCard}>
+      <div className={styles.card}>
+        <img src={show.photo} alt={show.name} className={styles.mainImg}/>
+      </div>
+      <span className={styles.name}>{show.title}</span>
+      <div className={styles.clubDate}>
+        <span className={styles.date}>{show.startDate}</span>
+        <span >{" - "}</span>
+        <span className={styles.date1}>{show.endDate}</span>
+      </div>
+    </div>
+  );
+}
+
+export default EventCard;
