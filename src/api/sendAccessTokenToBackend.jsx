a7df41f0e007a4c0b92dd2637237cbffbdbc0602
lingardd09 <sherlockicker@gmail.com>
2025-06-02 23:52:12 +0900

    feat: 로그인 코드 수정

--- src/api/sendAccessTokenToBackend.jsx
+++ src/api/sendAccessTokenToBackend.jsx
@@ -2,28 +2,22 @@
 import axios from "axios";
 
 const sendAccessTokenToBackend = async (idToken, navigate) => {
-  console.log("🔥 sendAccessTokenToBackend 호출됨");
   try {
-    const serverResponse = await axios.post(
+    const response = await axios.post(
       `${process.env.REACT_APP_DB_URL}/api/oauth/google/session`,
       { credential: idToken },
       {
-        headers: { "Content-Type": "application/json" },
-        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true, // 쿠키 필요시
       }
     );
-
-    console.log("📦 응답:", serverResponse.data);
-
-    // sessionStorage에 JWT 저장
-  
-    sessionStorage.setItem("jwt", serverResponse.data.token);
-    sessionStorage.setItem("serverResponse:Authority", serverResponse.data.authority);
-
-    navigate("/"); // 메인으로 이동
-    return serverResponse.data;
+    const jwt = response.data.token;
+    localStorage.setItem("jwt", jwt); // 저장 후 라우팅
+    navigate("/main");
   } catch (error) {
-    console.error("Login failed with error:", error);
+    console.error("Login failed with error: ", error);
     throw error;
   }
 };
