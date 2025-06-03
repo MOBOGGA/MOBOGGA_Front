445daa9c707f8abc48768536d1b83c5df143f62e
Haeun <hahashe@handong.ac.kr>
2025-06-04 01:20:06 +0900

    feat : 자잘한 오류들 수정

--- src/pages/ClubDetail.js
+++ src/pages/ClubDetail.js
@@ -8,7 +8,6 @@ import youtube from "../assets/icons/youtubeicons.svg";
 import kakao from "../assets/icons/kakao.svg"; 
 import link from "../assets/icons/linkicons.svg"; 
 
-import testImg from "../assets/recruitingTest/img1.png";
 
 import EventCard from "../components/ClubDetail/EventCard";
 import LastRecruitingCard from "../components/ClubDetail/LastRecruitingCard";
@@ -16,138 +15,7 @@ import LastEventCard from "../components/ClubDetail/LastEventCard";
 
 function ClubDetail() {
 
-  // const clubList = {
-    
-  //     clubId: 1,
-  //     clubName: "MIC",
-  //     photo: testImg,
-  //     mandatorySemesters: "연속 4학기",
-  //     content:
-  //       "   MIC는 Motion In Christ의 약자로 한동대 학생들로 구성된 춤을 통해 하나님께 영광 돌리는 청소년 사역단체입니다. 주로 힙합댄스를 기반으로 활동하며 힙합뿐만 아니라 왁킹, 걸스힙합, 락킹, 하우스, 크럼프, 댄스홀, 얼반 등 다양한 장르를 접할 수 있는 기회가 있습니다. MIC는 1996년에 창단되어 현재까지 이어져 오고 있으며, 학기 중 축제 및 스트릿 공연을 진행하고, MNT(mission and talent) 소속단체로서 겨울방학에는 태국으로 문화사역을 이어나갑니다. MIC는 Motion In Christ의 약자로 한동대 학생들로 구성된 춤을 통해 하나님께 영광 돌리는 청소년 사역단체입니다. 혹시 다양한 장르의 춤을 통해 사역하는 것이 궁금하신 분들이 계신가요? 춤을 한번도 춰보지 않았어도 끈기와 열정을 가지고 사역에 동참해 주실 동역자를 찾습니다. 주님안에 하나가 되어 저희의 가장 아름다울 청춘의 시간을 매우 값지게 보낼 수 있기를 원합니다!",
-  //     instaUrl: "https://www.instagram.com/motioninchrist_official",
-  //     youtubeUrl: "https://youtube.com/@motioninchrist",
-  //     kakaoUrl: "https://pf.kakao.com/_abcde",
-  //     url: "https://motioninchrist.co.kr",
-  //     activitySchedule: "활동일정\n• 정모시간: 매주 목요일 19시 ~ 21시\n• 3월 10일: 새학기 맞이 개강예배\n• 3월 25일: MTC 워크샵\n• 4월 06일 ~ 04월 12일: 리쿠르팅\n• 4월 20일: MTC 한동 연합 찬양집회\n• 5월 05일: MNT 말씀나눔 캠페인",
-  
-  //     progressingEventList: [
-  //       {
-  //         categoryOfEvent: "리쿠르팅",
-  //         id: 1,
-  //         photo: testImg,
-  //         title: "MIC 31ST 남자 추가 리쿠르팅",
-  //         startDate: "2025-03-23",
-  //         endDate: "2025-03-26"
-  //       },
-  //       {
-  //         categoryOfEvent: "워크샵",
-  //         id: 2,
-  //         photo: testImg,
-  //         title: "2025 MIC WORKSHOP",
-  //         startDate: "2025-03-11",
-  //         endDate: "2025-03-13"
-  //       },
-  //       {
-  //         categoryOfEvent: "공연",
-  //         id: 3,
-  //         photo: testImg,
-  //         title: "MIC Street Performance",
-  //         startDate: "2025-03-12",
-  //         endDate: "2025-03-12"
-  //       }
-  //     ],
-  
-  //     lastRecruitingList: [
-  //       {
-  //         recruitingId: 1,
-  //         photo: testImg,
-  //         period: "2024 - 1"
-  //       },
-  //       {
-  //         recruitingId: 2,
-  //         photo: testImg,
-  //         period: "2023 - 1"
-  //       },
-  //       {
-  //         recruitingId: 3,
-  //         photo: testImg,
-  //         period: "2022 - 1"
-  //       },
-  //       {
-  //         recruitingId: 4,
-  //         photo: testImg,
-  //         period: "2021 - 1"
-  //       }
-  //     ],
-  
-  //     lastEventList: [
-  //       {
-  //         showOrEntertain: "공연",
-  //         id: 1,
-  //         photo: testImg,
-  //         title: "2024 MIC 자체공연",
-  //         startDate: "2024-11-23",
-  //         endDate: "2024-11-24"
-  //       },
-  //       {
-  //         showOrEntertain: "공연",
-  //         id: 2,
-  //         photo: testImg,
-  //         title: "Street Verry Jam",
-  //         startDate: "2024-06-05",
-  //         endDate: "2024-06-05"
-  //       },
-  //       {
-  //         showOrEntertain: "전시",
-  //         id: 3,
-  //         photo: testImg,
-  //         title: "MNT 사진전 - 그리고",
-  //         startDate: "2024-03-31",
-  //         endDate: "2024-04-05"
-  //       },
-  //       {
-  //         showOrEntertain: "공연",
-  //         id: 4,
-  //         photo: testImg,
-  //         title: "2024 스트릿 공연",
-  //         startDate: "2024-03-13",
-  //         endDate: "2024-03-13"
-  //       },
-  //       {
-  //         showOrEntertain: "워크샵",
-  //         id: 5,
-  //         photo: testImg,
-  //         title: "2023-2 축제 워크샵",
-  //         startDate: "2023-11-01",
-  //         endDate: "2023-11-02"
-  //       },
-  //       {
-  //         showOrEntertain: "공연",
-  //         id: 6,
-  //         photo: testImg,
-  //         title: "2023 한스트 스트릿공연",
-  //         startDate: "2023-02-21",
-  //         endDate: "2023-02-21"
-  //       },
-  //       {
-  //         showOrEntertain: "워크샵",
-  //         id: 7,
-  //         photo: testImg,
-  //         title: "2022 MIC 워크샵",
-  //         startDate: "2022-03-12",
-  //         endDate: "2022-03-13"
-  //       },
-  //       {
-  //         showOrEntertain: "방송",
-  //         id: 8,
-  //         photo: testImg,
-  //         title: "2020 MIC 라이브방송",
-  //         startDate: "2020-04-29",
-  //         endDate: "2020-04-29"
-  //       }
-  //     ]
-    
-  //   };
+
 
     // 1) club 정보 데이터 가져오기 
       const [clubList, setClub] = useState([]);
