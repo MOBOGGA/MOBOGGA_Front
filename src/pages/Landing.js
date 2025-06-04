faef73b26cb7190421c16069139fc5f60f2a37f6
lingardd09 <sherlockicker@gmail.com>
2025-06-05 01:36:22 +0900

    feat: 랜딩페이지에서 바로 구글 로그인 창 뜨게

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -19,7 +19,29 @@ function Landing() {
   };
 
   const onClickGoogleStartBtn = () => {
-    navigate(`/login`);
+    const state = Math.random().toString(36).substring(2);
+    sessionStorage.setItem("oauth_state", state);
+
+    const nonce =
+      Math.random().toString(36).substring(2) + Date.now().toString(36);
+
+    const authUrl = new URL("https://accounts.google.com/o/oauth2/v2/auth");
+    authUrl.searchParams.append(
+      "client_id",
+      process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID
+    );
+    authUrl.searchParams.append(
+      "redirect_uri",
+      process.env.REACT_APP_GOOGLE_AUTH_REDIRECT_URI
+    );
+    authUrl.searchParams.append("response_type", "id_token");
+    authUrl.searchParams.append("scope", "email profile openid");
+    authUrl.searchParams.append("nonce", nonce);
+    authUrl.searchParams.append("access_type", "offline");
+    authUrl.searchParams.append("prompt", "consent");
+    authUrl.searchParams.append("state", state);
+
+    window.location.href = authUrl.toString();
   };
 
   return (
