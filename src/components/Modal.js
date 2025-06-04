56fd80b1cf3a4590e1f466e811da8749661d2295
junnyang <lhr8440@gmail.com>
2025-06-04 15:17:09 +0900

    feat: 모달구현시도

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,3 @@
-import { IoClose } from "react-icons/io5";
-
 const Modal = ({ isOpen, onClose, children }) => {
   // 만약 isOpen이 false이면 null을 반환하여 모달을 렌더링하지 않음
   if (!isOpen) return null;
@@ -7,9 +5,7 @@ const Modal = ({ isOpen, onClose, children }) => {
   return (
     <div onClick={onClose} className="modal-overlay">
       <div onClick={(e) => e.stopPropagation()} className="modal">
-        <button onClick={onClose} className="modal-close">
-          <IoClose />
-        </button>
+        <button onClick={onClose} className="modal-close"></button>
         {children}
       </div>
     </div>
